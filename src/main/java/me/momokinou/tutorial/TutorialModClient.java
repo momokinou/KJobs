dead7c7915fb36ed525d008f829e286f58c624b0
momokinou <moriceau.q@gmail.com>
2023-05-27 18:29:52 +0200

    initial

---
+++ src/main/java/me/momokinou/tutorial/TutorialModClient.java
@@ -0,0 +1,10 @@
+package me.momokinou.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
