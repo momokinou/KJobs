b8aedafe8eed48319cd901107315ec379abb23ac
momokinou <moriceau.q@gmail.com>
2023-05-31 15:57:19 +0200

    feat: new block

--- src/main/java/me/momokinou/tutorial/TutorialMod.java
+++ src/main/java/me/momokinou/tutorial/TutorialMod.java
@@ -1,5 +1,6 @@
 package me.momokinou.tutorial;
 
+import me.momokinou.tutorial.block.ModBlocks;
 import me.momokinou.tutorial.item.ModItemGroup;
 import me.momokinou.tutorial.item.ModItems;
 import net.fabricmc.api.ModInitializer;
@@ -14,5 +15,6 @@ public class TutorialMod implements ModInitializer {
     public void onInitialize() {
         ModItemGroup.registerItemsGroups();
         ModItems.registerModItems();
+        ModBlocks.registerModBlocks();
     }
 }
